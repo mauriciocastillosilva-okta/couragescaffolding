9b54c305b917875ff02a892831843864920324eb
Frank Montanaro <frank.montanaro@okta.com>
2014-08-15 13:48:36 -0700

    Changing to Base and adding prompt for project name

--- generator-courage/app/index.js
+++ generator-courage/app/index.js
@@ -1,12 +1,28 @@
 var yeoman = require('yeoman-generator');
 
-module.exports = yeoman.generators.NamedBase.extend({
+module.exports = yeoman.generators.Base.extend({
   initializing: {
 		projectName: function () {
       console.log('Creating files for new project ' + this.name);
 	  }
   },
-
+  prompting: {
+     projectName: function() {
+     	var done = this.async();
+	        this.prompt({
+	        	type    : 'input',
+	      		name    : 'name',
+	     		message : 'Your project name'
+	     	}, function (answers) {
+	     	if( answers.name === "" ) {
+	     		throw { name: 'FatalError', message: 'You must define project name.' };
+	     	} 
+		    this.name = answers.name;
+		    this.log("ProjectName = " + this.name);
+		     done();
+		    }.bind(this));
+	 }
+  },
   writing: {
 	  copyTemplates: function () {
         this.template('../../templates/Controller.tpl.js', this.name + 'Controller.js');

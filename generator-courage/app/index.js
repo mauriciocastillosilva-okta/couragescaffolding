8ce5d725ca8dd0e868e2358bb87fee37e3a11221
Frank Montanaro <frank.montanaro@okta.com>
2014-08-15 15:11:50 -0700

    Adding in parameter name instead of prompt

--- generator-courage/app/index.js
+++ generator-courage/app/index.js
@@ -1,53 +1,61 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+    this.argument('new', { type: String, required: true });
+    this.argument('delete', { type: String, required: false });
+
+    // setup project name
+    this.name = this.new
+
+  },
   initializing: {
 		projectName: function () {
-      console.log('Creating files for new project ' + this.name);
+      		this.log('Creating files for new project ' + this.name);
 	  }
   },
   prompting: {
      prompts: function() {
-     	var done = this.async();
-     	var prompts = [{
-                type    : 'input',
-	      		name    : 'name',
-	     		message : 'Your project name'
-     	},{
-     	        type    : 'input',
-	      		name    : 'api',
-	     		message : 'API endpoint',
-	     		default : '/change/this/endpoint'
-	    },{
-	    		type    : 'input',
-	      		name    : 'formTitle',
-	     		message : 'Form Title',
-	     		default : 'FORM_TITLE'
-	    }];
+      this.log(this.yeoman);
 
-	    this.prompt(prompts, function (answers) {
-	    	// Check for required project name
-        	if( answers.projectName === "" ) {
-	     		throw { name: 'FatalError', message: 'You must define project name.' };
-	     	} 
-		    this.name = answers.name;
-		    this.log("ProjectName = " + this.name);
+	     	var done = this.async();
+	     	var prompts = [{
+	     	    type    : 'input',
+		      	name    : 'api',
+		     		message : 'API endpoint',
+		     		default : '/change/this/endpoint'
+		    },{
+		    		type    : 'input',
+		      		name    : 'formTitle',
+		     		message : 'Form Title',
+		     		default : 'FORM_TITLE'
+		    }];
 
-		    //check for optional inputs
-		    this.api = answers.api;
-		    this.formTitle = answers.formTitle;
+		    this.prompt(prompts, function (answers) {
+		    	// Check for required project name
+	        	if( answers.projectName === "" ) {
+		     		throw { name: 'FatalError', message: 'You must define project name.' };
+		     	} 
+			    this.name = answers.name;
+			    this.log("ProjectName = " + this.name);
 
-		    done();
+			    //check for optional inputs
+			    this.api = answers.api;
+			    this.formTitle = answers.formTitle;
 
-	    }.bind(this));
-	}
+			    done();
+
+		    }.bind(this));
+		}
   },
   writing: {
 	  copyTemplates: function () {
-        this.template('../../templates/Controller.tpl.js', this.ctor + 'Controller.js');
-        this.template('../../templates/main-.tpl.js', 'main-' + this.proj + '.js');
-        this.template('../../templates/models/Model.tpl.js', 'views/' + this.ctor + '.js');
-        this.template('../../templates/views/View.tpl.js', 'models/' + this.ctor + '.js');
+	  	this.template('../../templates/Controller.tpl.js', this.ctor + 'Controller.js');
+	    this.template('../../templates/main-.tpl.js', 'main-' + this.proj + '.js');
+	    this.template('../../templates/models/Model.tpl.js', 'views/' + this.ctor + '.js');
+	    this.template('../../templates/views/View.tpl.js', 'models/' + this.ctor + '.js');
+	  	
 	  }
   },
 
@@ -61,7 +69,7 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  proj: function () { return this.util.lowercase(this.name)},
-  ctor: function () { return this.util.capitalize(this.name)},
+  proj: function () { if(this.name) {return this.util.lowercase(this.name)}},
+  ctor: function () { if(this.name) {return this.util.capitalize(this.name)}},
 
 });

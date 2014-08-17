5b3ff7805e4344d382f81f2b674ad3cafdbbc9d3
Mauricio Castillo <mauricio.castillosilva@okta.com>
2014-08-17 00:28:43 -0700

    HelloWorld should work now
    Removed name prompt (didn't seem necessarry since we are creating a hello world)
    yo courage:helloworld (it will ask only for a target directory)

--- generator-courage/helloworld/index.js
+++ generator-courage/helloworld/index.js
@@ -2,14 +2,6 @@ var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
 
-  constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);
-    this.argument('new', { type: String, required: false });
-
-    // setup project name from argument
-    this.name = this.new;
-  },
-
   initializing: {
     logo: function() {
       this.log('                                     _____');
@@ -28,28 +20,7 @@ module.exports = yeoman.generators.Base.extend({
       this.log('                                       |  \\_((_(_)|/(_)');
       this.log('                                       \\             (');
       this.log('                                        \\_____________');
-      this.log('\n\nDemo');
-    },
-    projectNamePrompt: function () {
-      // If the user didn't specify a name for --new, prompt here for it
-      if(typeof this.name === 'undefined' || this.name === null || this.name === "") {
-        var pName = this.async();
-        var pPrompt = [{
-            type    : 'input',
-            name    : 'name',
-            message : 'Project Name',
-        }];
-        var self = this;
-        this.prompt(pPrompt, function (answers) {
-            // If they still refuse, quit
-            if( answers.name === "" ) {
-              throw { name: 'FatalError', message: 'You must define project name.' };
-            }
-            self.name = answers.name;
-            pName();
-        });
-      }
-
+      this.log('\n\nHelloWorld - Where is Uzi?');
     }
   },
 
@@ -64,9 +35,6 @@ module.exports = yeoman.generators.Base.extend({
       }];
 
       this.prompt(prompts, function (answers) {
-
-        this.log("Project Name = " + this.name);
-
         //check for optional inputs
         this.target = answers.target;
 
@@ -79,43 +47,31 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
     copyTemplates: function () {
       var targetDir = this.getTargetDir();
-      this.template('HelloWorldController.tpl.js', targetDir + this.ctor() + 'Controller.js');
-      this.template('models/User.tpl.js', targetDir + 'User.js');
-      this.template('views/HelloWorld.tpl.js', targetDir + this.ctor() + '.js');
-      this.template('main-helloworld.tpl.js', targetDir + "main-" + this.proj() + '.js');
+      this.template('HelloWorldController.tpl.js', targetDir + 'HelloWorldController.js');
+      this.template('main-helloworld.tpl.js', targetDir + 'main-helloworld.js');
+      this.template('models/User.tpl.js', targetDir + 'models/User.js');
+      this.template('views/HelloWorld.tpl.js', targetDir + 'views/HelloWorld.js');
     },
   },
 
-  util: {
-    lowercase: function (word) {
-      return word.toLowerCase();
-    },
-
-    capitalize: function (word) {
-      return word[0].toUpperCase() + word.slice(1);
-    }
-  },
-
-  proj: function () { if(this.name) {return this.util.lowercase(this.name)}},
-  ctor: function () { if(this.name) {return this.util.capitalize(this.name)}},
   getTargetDir: function () {
     if (this.target.length > 0 && this.target.substr(this.target.length-1) !== '/') {
       this.target += '/';
     }
-    return this.target + this.proj() + '/';
+    return this.target + 'helloworld/';
   },
 
   end: {
     jspOutput: function () {
-      this.log('Demo project created.');
-      this.log('Copy the next lines to the jsp where you need to include this project');
+      this.log('Demo HelloWorld created.');
+      this.log('Copy the next lines to the jsp if you want to see HelloWorld in action');
       this.log('');
-      this.log('  <div id="'+ this.proj() + '-container">');
-      this.log('    <ss:requirejs main="' + this.proj() + '/main-' + this.proj() + '">');
+      this.log('  <div id="helloworld-container">');
+      this.log('    <ss:requirejs main="helloworld/main-helloworld">');
       this.log('    <script>');
       this.log('      require.config || (require.config = {});');
       this.log('      require.config[\'config\'] = {');
-      this.log('        el: \'#' + this.proj() + '-container\'');
+      this.log('        el: \'#helloworld-container\'');
       this.log('      };');
       this.log('    </script>');
       this.log('    </ss:requirejs>');
